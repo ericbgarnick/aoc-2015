e4b9e07399847430f1939ae7aef4d09ecfc9f63f
Eric Garnick <eric@energysage.com>
2020-11-10 19:32:09 -0500

    Day 6 part 1 done (but slow and breaks compiler)

--- src/aoc_2015/day06/solution1.clj
+++ src/aoc_2015/day06/solution1.clj
@@ -1,5 +1,11 @@
 (ns day06.solution1
+  (:require [clojure.string :refer [split]])
   (:gen-class))
+
+(defn parse-input
+  "Read input file content and split on newlines"
+  []
+  (split (slurp "src/aoc_2015/day06/input.txt") #"\n"))
 
 ;;;;;;
 ;; Manipulate grid values
@@ -19,25 +25,48 @@
 ;;;;;;
 ;; Interpret instructions
 ;;;;;;
-(defn set-points
-  [grid [x-start y-start] [x-end y-end] operation]
+(defn points-for-range
+  [[x-start y-start] [x-end y-end]]
   (for [x-val (range x-start (+ x-end 1))
         y-val (range y-start (+ y-end 1))]
-    (operation grid [x-val y-val])))
+    [x-val y-val]))
 
-;; TODO: get operation function by name
-(defn extract-operation
+(defn set-points
+  [grid start-point end-point operation]
+  (reduce operation grid (points-for-range start-point end-point)))
+
+(defn operation-name
   [instruction]
   (clojure.string/replace (clojure.string/trim (re-find #"[a-z ]+" instruction)) #" " "-"))
 
+(defn extract-operation
+  [instruction]
+  (-> (operation-name instruction) symbol resolve))
+
 (defn extract-start-point
   [instruction]
-  (clojure.string/split (re-find #"\d+,\d+" instruction) #","))
+  (map #(Integer/parseInt %) (clojure.string/split (re-find #"\d+,\d+" instruction) #",")))
 
 (defn extract-end-point
   [instruction]
-  (clojure.string/split (re-find #"\d+,\d+$" instruction) #","))
+  (map #(Integer/parseInt %) (clojure.string/split (re-find #"\d+,\d+$" instruction) #",")))
 
 (defn execute-line
   [line grid]
   (set-points grid (extract-start-point line) (extract-end-point line) (extract-operation line)))
+
+(defn execute-instructions
+  [instructions]
+  (loop [instructions instructions
+         grid {}]
+    (if (empty? instructions)
+      grid
+      (recur (rest instructions) (execute-line (first instructions) grid)))))
+
+(defn count-switched-on
+  [grid]
+  (count (filter true? (map val grid))))
+
+(defn -main
+  [& args]
+  (println (count-switched-on (execute-instructions (parse-input)))))
